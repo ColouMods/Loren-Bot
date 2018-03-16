bbee5cb4b07f1328938531869481298775ddb17b
ColouMods <32771001+ColouMods@users.noreply.github.com>
2018-03-16 22:26:36 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('I am ready!');
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-  if (message.content === '@Loren Bot') {
-    message.reply('Oh yeah the game is hardcodedly hiding those meshes for rocke_v. When you rename the car it no longer does that.');
-  }
+    if (message.content === '@Loren Bot') {
+    	message.reply('Oh yeah the game is hardcodedly hiding those meshes for rocke_v. When you rename the car it no longer does that.');
+  	}
 });
 
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
