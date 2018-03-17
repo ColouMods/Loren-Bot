2ff08455843a405b3de5e6b50ceeb3c111ddece5
ColouMods <32771001+ColouMods@users.noreply.github.com>
2018-03-17 01:15:22 +0000

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ client.on('message', message => {
     if (message.content === '@Loren Goodwin') {
         message.channel.sendMessage('Oh yeah the game is hardcodedly hiding those meshes for rocke_v. When you rename the car it no longer does that.');
     if (message.content.match(/steamed hams/i)) {
-    message.channel.sendMessage('Can we cool it on the constant Steamed Hams references in this chat?');
-}
+        message.channel.sendMessage('Can we cool it on the constant Steamed Hams references in this chat?');
   	}
 });
 
