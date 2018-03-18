04a87be338c9251e569451f86384ab6887189faa
DocBison <37480191+DocBison@users.noreply.github.com>
2018-03-18 09:34:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ client.on('message', message => {
 	{
         message.channel.sendMessage("I've never seen the Lion King.");
         }
-		else if (message.content.match(/why aren't the wheels transparent/i) || (message.content.match(/wheels/i) && (message.content.match(/transparent/i))
+		else if (message.content.match(/wheels/i) || (message.content.match(/transparent/i))
 	{
 	message.channel.sendMessage("Oh yeah the game is hardcodedly hiding those meshes for rocke_v. When you rename the car it no longer does that.");
 	}
