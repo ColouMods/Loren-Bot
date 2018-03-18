f87792da9a272f1920ce9cae77b389fbdaefce1e
DocBison <37480191+DocBison@users.noreply.github.com>
2018-03-17 20:00:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.setStatus('online', 'SketchUp');
+    client.setPlayingGame("SketchUp");
     console.log('I am ready!');
 });
 
