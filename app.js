2c8d9be268aadea6c72c01f6d3ec5682668763f6
DocBison <37480191+DocBison@users.noreply.github.com>
2018-03-18 12:48:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ client.on('message', message => {
     
 	
 	//This is basic content searching
-    if (message.content.match(/status/i)||(message.content.match(/update/i)) 
+    //if (message.content.match(/status/i)||(message.content.match(/update/i)) 
     //{
 	//var d = Date.Now();
 	//var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
