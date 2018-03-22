5aaa6dcd876a190e765dee2da2376b53a2d469ad
DocBison <37480191+DocBison@users.noreply.github.com>
2018-03-22 12:06:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ client.on('message', message => {
    //greeting code
 if (message.content.match(/hi loren/i))
     {
-    message.channel.sendMessage("Hi whoever-you-are. Are you ${message.author.avatarURL}?");
+    message.channel.sendMessage("Hi whoever-you-are. Are you " + message.author.displayName +"?");
     }
 
 	//This is basic content searching
