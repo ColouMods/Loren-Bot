1ae19bb1397741364c655d83eb65a750d1c9d8b4
DocBison <37480191+DocBison@users.noreply.github.com>
2018-03-22 11:57:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ client.on('message', message => {
    //greeting code
 if (message.content.match(/hi loren/i))
     {
-    message.channel.sendMessage("Hi @424318662674087946.");
+    message.channel.sendMessage("Hi whoever-you-are.");
     }
 
 	//This is basic content searching
