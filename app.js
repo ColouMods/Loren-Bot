28f20138b04c907b9d460fbf7a6e7a975f696f3d
DocBison <37480191+DocBison@users.noreply.github.com>
2018-03-28 19:16:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ const Discord = require('discord.js');
          message.content.match(/despite your directions/i)
          )
      {
-         message.channel.sendMessage('Can we cool it on the constant Steamed Hams references in this chat?',{tts: true});
+         message.channel.sendMessage('Can we cool it on the constant Steamed Hams references in this chat?');
  	return;
      }
     //greeting code
