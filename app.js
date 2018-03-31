be8998a5f41adc149fa53c752dc84fb3cad587ac
DocBison <37480191+DocBison@users.noreply.github.com>
2018-03-31 11:34:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ const Discord = require('discord.js');
      {
          message.channel.sendMessage("G2A is incredibly sketchy and I wouldn't put my money anywhere near that website.");
  	   return;
+     if (message.content.match(/80s cars mixed/i) ||
+         message.content.match(/80's cars mixed/i)||
+         (message.content.match(/ecto/i) && message.content.match(/delorean/i))
+         )
+     {
+         message.channel.sendMessage("G2A is incredibly sketchy and I wouldn't put my money anywhere near that website.");
+ 	   return;
      }
      if (message.content.match(/steamed hams/i) ||
          message.content.match(/aurora borealis/i) ||
