0fb6c0a5756ee13a8476969e082bcc24f15d82cc
DocBison <37480191+DocBison@users.noreply.github.com>
2018-04-06 12:49:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ const Discord = require('discord.js');
      {
          message.channel.sendMessage("G2A is incredibly sketchy and I wouldn't put my money anywhere near that website.");
  	   return;
+     if (message.content.match(/slippery hill/i)
+         )
+     {
+         message.channel.sendMessage("pls explain slippery hill joke");
+         setTimeout(() => {
+				         message.channel.sendMessage("^ there's a good line for Loren Bot whenever anyone says that.");
+			      }, 2000);
+ 	   return;
      }
      if (message.content.match(/80s cars mixed/i) ||
          message.content.match(/80's cars mixed/i) ||
