8f3ce243dc57e5481185efe5a83ac925b70f58a0
DocBison <37480191+DocBison@users.noreply.github.com>
2018-04-21 12:28:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ client.on('message', message => {
      var second2 = second;
      });*/
     if (message.content.match(/siivagunner/i) ||
-        message.content.match(/silvagunner/i)
+        message.content.match(/silvagunner/i) ||
+        message.content.match(/please read the channel description/i)
     ) {
         message.channel.sendMessage(':ok_hand::eyes::ok_hand::eyes::ok_hand::eyes::ok_hand::eyes::ok_hand::eyes: good shit go౦ԁ sHit:ok_hand: thats :heavy_check_mark: some good:ok_hand::ok_hand:shit right:ok_hand::ok_hand:there:ok_hand::ok_hand::ok_hand: right:heavy_check_mark:there :heavy_check_mark::heavy_check_mark:if i do ƽaү so my self :100: i say so :100: thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ:100: :ok_hand::ok_hand: :ok_hand:НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ:ok_hand: :ok_hand::ok_hand: :ok_hand: :100: :ok_hand: :eyes: :eyes: :eyes: :ok_hand::ok_hand:Good shit');
         return;
