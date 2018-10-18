0d0306d895475baaf39ddb26ca8023b204db6f2e
DocBison <37480191+DocBison@users.noreply.github.com>
2018-10-18 14:20:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -157,8 +157,9 @@ client.on('message', message => {
         var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         var month = months[update.getMonth()];
         var year = update.getFullYear();
-        message.channel.sendMessage("Hey everyone, this is Loren from Donut Team. Welcome to the " + month + " " + year + " Status Update." + updates[1]);
-        return;
+        //message.channel.sendMessage("Hey everyone, this is Loren from Donut Team. Welcome to the " + month + " " + year + " Status Update." + updates[parseInt(updateVariant)]);
+        message.channel.sendMessage("Hey everyone, this is Loren from Donut Team. Welcome to the " + month + " " + year + " Status Update.");
+	return;
     }
 	
     if (message.content.match(/status/i) || message.content.match(/update/i)) {
